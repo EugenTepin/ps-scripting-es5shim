028388638bb6093a18e973e186481242d5016c1f
Eugen Tepin <4godwork@gmail.com>
2016-09-24 19:04:43 +0300

    Make npm module
    Initial adjustments
    install webpack for creating bundles

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ps-scripting-es5shim
-ECMAScript 5 compatibility shims for Adobe Photoshop JavaScript engine
+ECMAScript 5 compatibility shims for Adobe Photoshop JavaScript engine.
+
+**Under development!**
---
+++ index.js
@@ -0,0 +1,41 @@
+/*
+TODO
+what need to shim?
+
+Function
+Function.bind [done by mdn]
+
+
+Object
+• Object.defineProperty
+• Object.defineProperties(obj, propName, descriptorSet)
+• Object.create(protoObj, descriptorSet)
+• Object.getOwnPropertyNames(obj)
+• Object.Prototype
+• Object.getPrototypeOf(obj)
+• Object.getOwnPropertyDescriptor
+• Object.getOwnPropertyNames
+• Object.preventExtensions
+• Object.isExtensible(obj)
+• Object.seal
+• Object.isSealed(obj)
+• Object.freeze
+• Object.isFrozen(obj)
+• Object.keys
+
+Array
+• Array.prototype.indexOf
+• Array.prototype.lastIndexOf
+• Array.prototype.every
+• Array.prototype.some
+• Array.prototype.forEach
+• Array.prototype.map
+• Array.prototype.filter
+• Array.prototype.reduce
+• Array.prototype.reduceRight
+
+JSON
+• JSON.stringify()
+• JSON.parse()
+
+*/
