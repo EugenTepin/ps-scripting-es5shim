d5f4b71589a0f54b90948780be2df6f37caa42f4
Eugen Tepin <4godwork@gmail.com>
2016-09-28 01:30:32 +0300

    Demo: webpack as bundler part2

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,17 @@ var webpack = require("webpack");
 
 module.exports = {
     entry: {
-          main:  ['./index.js'],
-          test:  ['./index.js','./test.js']
+          main:  ['./index.js'], // main bundle it collects all shims together
+          test:  ['./index.js','./test.js'] // this bundle include some tests and shims wich loads in first place
           },
     output: {
-            path: __dirname + '/bundle',
+            path: __dirname + '/bundle', // where to store bundles
             filename: '[name].bundle.js',
         },
 
     
     module: {
+      // you can use babel for supporting es2015 
       /*  loaders: [
             {
               test: /\.jsx?$/,
@@ -23,11 +24,12 @@ module.exports = {
             }
         ]*/
     },
+    // and you can use compression (well, almost can)
         plugins: [
                 new webpack.optimize.UglifyJsPlugin({
-                    comments: true,
-                    compress: false,
-                    beautify: true
+                    comments: true, // it is true for demo purposes only
+                    compress: false, // must be false, because code may become so ugly that Photoshop will throw error :)
+                    beautify: true // it is true for demo purposes only
 
                 })
             ]
